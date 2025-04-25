07689e4b4341d6ef2c8094a3cd132c35038a1f69
Moonies <bungtwoza@gmail.com>
2025-04-25 16:58:53 +0900

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-import mdx from "@next/mdx";
+import mdx from '@next/mdx'
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
   // options: {},
-});
-
+})
 
 export default withMDX({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  transpilePackages: ["next-mdx-remote"], //for fix latest version
-});
+  transpilePackages: ['next-mdx-remote'], //for fix latest version
+})
