e611e686913b0765b6851889c0b2eb4b3e050f7c
Moonies <bungtwoza@gmail.com>
2025-04-28 11:06:18 +0900

    add type config

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,16 @@ const withMDX = mdx({
   // options: {},
 })
 
-export default withMDX({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   transpilePackages: ['next-mdx-remote'], //for fix latest version
-})
+}
+
+export default withMDX(nextConfig)
