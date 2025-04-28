73ff1d40b5c06b1f426a61110aa89b06f639b4ca
Moonies <bungtwoza@gmail.com>
2025-04-28 09:07:14 +0900

    remove disDir

--- next.config.mjs
+++ next.config.mjs
@@ -14,5 +14,4 @@ export default withMDX({
   },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   transpilePackages: ['next-mdx-remote'], //for fix latest version
-  distDir: 'build',
 })
