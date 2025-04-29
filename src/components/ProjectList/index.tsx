7071dea05302966bca54cf76720e0208c0829af4
Moonies <bungtwoza@gmail.com>
2025-04-29 12:00:02 +0900

    change location

--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -4,30 +4,32 @@ import { ProjectCard } from '@/components/ProjectCard'
 
 interface ProjectsProps {
   range?: [number, number?]
-  // lang: string
+  lang: string
 }
 
-export async function generateStaticParams(): Promise<{ slug: string; lang: string }[]> {
-  const languages = ['en', 'jp']
-  const allPosts: { slug: string; lang: string }[] = []
+// export async function generateStaticParams(): Promise<{ slug: string; lang: string }[]> {
+//   const languages = ['en', 'jp']
+//   const allPosts: { slug: string; lang: string }[] = []
 
-  // Fetch posts for each locale
-  for (const lang of languages) {
-    const posts = getPosts(['src', 'app', '[lang]', 'work', 'projects', lang])
-    allPosts.push(
-      ...posts.map(post => ({
-        slug: post.slug,
-        lang: lang,
-      }))
-    )
-  }
+//   // Fetch posts for each locale
+//   for (const lang of languages) {
+//     const posts = getPosts(['src', 'app', '[lang]', 'work', 'projects', lang])
+//     allPosts.push(
+//       ...posts.map(post => ({
+//         slug: post.slug,
+//         lang: lang,
+//       }))
+//     )
+//   }
 
-  return allPosts
-}
+//   return allPosts
+// }
 
 export default async function Projects(param: ProjectsProps) {
-  // const { lang } = await param
-  const allProjects = getPosts(['src', 'app', '[lang]', 'work', 'projects', 'en'])
+  const { lang } = await param
+  // const allProjects = getPosts(['src', 'app', '[lang]', 'work', 'projects', 'en'])
+  const allProjects = getPosts([lang, 'work'])
+
   console.log(allProjects)
   if (!allProjects.length) {
     return <div>No projects found.</div>
